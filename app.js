f9891ed4db3b6e6c5825d92ac729760988f8d361
Manan Guglani <manan26.mg@gmail.com>
2020-12-14 03:44:17 +0530

    CRUD

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 
 const dbURI = 'mongodb+srv://Manan:Xifo4DchOfTiGfOv@cluster0.fwvnk.mongodb.net/users?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(result => app.listen(3000))
+    .then(result => app.listen(process.env.PORT || 3000))
     .catch(err => console.log(err));
 
 app.set('view engine', 'ejs');
